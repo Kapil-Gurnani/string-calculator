b2b72f5114b612bbbe84cac431aeab556c3d94c8
Kapil-Gurnani <kapilgurnani4@gmail.com>
2024-08-10 23:22:01 +0530

    multiple delimitors(commas and \n)

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -8,4 +8,8 @@ describe('String Calculator', () => {
         expect(add("1,2,3")).toBe(6);
         expect(add("1,4,6,7,8,9,0,4,3,2")).toBe(44);
     });
+    test(', and \n as delimiters', () => {
+        expect(add("1\n2,3")).toBe(6);
+        expect(add("1,2,\n")).toBe("Invalid input format");
+    });
 });
