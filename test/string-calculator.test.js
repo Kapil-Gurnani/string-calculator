6df46a5f297d93d88a3af87118f16492054db7bc
Kapil-Gurnani <kapilgurnani4@gmail.com>
2024-08-10 23:12:12 +0530

    multiple positive numbers will be passed with comma separators

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -3,5 +3,9 @@ const { add } = require("../src/string-calculator");
 describe('String Calculator', () => {
     test('Check Empty String', () => {
         expect(add("")).toBe(0);
-    })
+    });
+    test('Check Sum of positive numbers', () => {
+        expect(add("1,2,3")).toBe(6);
+        expect(add("1,4,6,7,8,9,0,4,3,2")).toBe(44);
+    });
 });
